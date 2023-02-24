da02a5658e179d92121b09f224582980e735ab43
Syed25794 <syedgulmohammad25794@gmail.com>
2023-02-24 08:58:03 +0530

    fetch the image url

--- client/src/ImageGenaration.jsx
+++ client/src/ImageGenaration.jsx
@@ -4,10 +4,20 @@ import { Input , Image, Button } from '@chakra-ui/react'
 const ImageGenaration = () => {
     const [query,setQuery]=useState("");
     const [imageUrl,setImageUrl]=useState("");
-    const generateImage=()=>{
-      console.log(query);
-      setImageUrl(query);
+    const generateImage=async ()=>{
+      try {
+        let response = await fetch("http://localhost:8080/generateImage",{
+          method:"POST",
+          body:{text:query,number:1,size:"1024x1024"},
+          headers:{"Content-Type":"application/json"}
+        });
+        let result = await response.json();
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
     }
+
   return (
     <div>
         <Input marginTop="20px" w="850px" type="text" placeholder="Generate Image with text" name="ImageText" value={query} onChange={(e)=>setQuery(e.target.value)} />
