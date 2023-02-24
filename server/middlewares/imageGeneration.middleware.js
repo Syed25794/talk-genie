b4c0ab91a58d23464f4d952d94d10865fba503dd
Syed25794 <syedgulmohammad25794@gmail.com>
2023-02-24 08:27:29 +0530

    added middleware and refactor the code

---
+++ server/middlewares/imageGeneration.middleware.js
@@ -0,0 +1,16 @@
+const ImageGenarationMiddlewares = async ( req, res , next )=>{
+    const { text, size, number }=req.body;
+    if( !text ){
+        return res.send({message:"Text is required for generating an image."});
+    }else{
+        if( +number < 1 || +number > 10 ){
+            return res.send({message:"Image number should be from 1 to 10."});
+        }
+        if( size !== '1024x1024' || size !== '256x256' || size !== '512x512' ){
+            return res.send({message:"Image size should be either '1024x1024' or '512x512' or '256x256'."});
+        }
+    }
+    next();
+}
+
+module.exports = ImageGenarationMiddlewares;
--- server/routes/image.routes.js
+++ server/routes/image.routes.js
@@ -1,23 +1,8 @@
 const { Router } = require("express");
-require("dotenv").config();
-const { Configuration, OpenAIApi } = require("openai");
-const configuration = new Configuration({
-  apiKey: process.env.OPEN_API_KEY
-});
-const openai = new OpenAIApi(configuration);
+const { generateImage } = require("../actions/image.actions");
+const ImageGenarationMiddlewares = require("../middlewares/imageGeneration.middleware");
 const imageRoutes = Router();
 
-imageRoutes.post("/generateImage",async(req,res)=>{
-    const { text, number,size }=req.body;
-    const response = await openai.createImage({
-        prompt:text,
-        n: Number( number ),
-        size:size,
-      });
-      image_url = response.data.data[0].url;
-    res.send({url:image_url});
-});
-
-
+imageRoutes.post("/generateImage",ImageGenarationMiddlewares,generateImage);
 
 module.exports = imageRoutes;
