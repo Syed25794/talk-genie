b78c29ccb9bef44f31eaeb53888d76e95d9001be
Syed25794 <syedgulmohammad25794@gmail.com>
2023-02-24 10:20:46 +0530

    fixed some bugs

--- server/actions/image.actions.js
+++ server/actions/image.actions.js
@@ -1,23 +1,29 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey:"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"
+  apiKey:"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"
 });
 
 const openai = new OpenAIApi(configuration);
 
-const generateImage=(req,res)=>{
+const generateImage=async(req,res)=>{
     const { text, number,size }=req.body;
-    openai.createImage({
-      prompt:text,
-      n: +number,
-      size:size,
-    }).then((response)=>{
+    if( !number ){
+      number=1;
+    }
+    if( !size ){
+      size='1024x1024';
+    }
+    try {
+      let response = await openai.createImage({
+        prompt:text,
+        n: +number,
+        size:size,
+      });
       res.send({data:response.data.data});
-    })
-    .catch((err)=>{
-      res.send({error:err});
-    });
+    } catch (error) {
+      res.send({error});
+    };
 };
 
 
--- server/middlewares/imageGeneration.middleware.js
+++ server/middlewares/imageGeneration.middleware.js
@@ -7,6 +7,7 @@ const ImageGenarationMiddlewares = async ( req, res , next )=>{
             return res.send({message:"Image number should be from 1 to 10."});
         }
         if( size !== "1024x1024" && size !== "256x256" && size !== "512x512" ){
+            console.log(size !== "1024x1024" , size !== "256x256" , size !== "512x512");
             return res.send({message:"Image size should be either '1024x1024' or '512x512' or '256x256'."});
         }
     }
